6bc312e968ef803575327b54a25747e8163013bb
fidannh <fidannh@code.edu.az>
2024-01-23 12:20:47 +0400

    Completed

---
+++ StudentsApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using StudentsApp;
+using System;
+  bool isValidInput = false;
+
+        do
+        {
+            Student student = new Student();
+
+            Console.Write("Enter Fullname: ");
+            student.Fullname = Console.ReadLine();
+
+            Console.Write("Enter GroupNo: ");
+            student.GroupNo = Console.ReadLine();
+
+            Console.Write("Enter Age: ");
+            string ageInput = Console.ReadLine();
+
+            if (byte.TryParse(ageInput, out byte age) &&
+                !string.IsNullOrWhiteSpace(student.Fullname) &&
+                Student.CheckGroupNo(student.GroupNo))
+            {
+                student.Age = age;
+                isValidInput = true;
+                Console.WriteLine($"Fullname: {student.Fullname} - Age: {student.Age} - GroupNo: {student.GroupNo}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please try again!!.");
+            }
+
+        } while (!isValidInput);
+    
+
---
+++ StudentsApp/Student.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace StudentsApp
+{
+    public class Student
+    {
+        private string _fullname;
+        private byte _age;
+        private string _groupNo;
+
+        public byte Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+        public string Fullname
+        {
+            get { return _fullname; }
+            set
+            {
+                while (!CheckFullname(value))
+                {
+                    Console.WriteLine("Invalid fullname format. Please enter again!!.");
+                    Console.Write("Enter Fullname: ");
+                    value = Console.ReadLine();
+                }
+                _fullname = value;
+            }
+        }
+
+        private static bool CheckFullname(string fullname)
+        {
+            return !string.IsNullOrWhiteSpace(fullname) && fullname.Split(' ').Length == 2;
+        }
+
+
+        public static bool CheckGroupNo(string groupNo)
+        {
+            if (groupNo.Length != 4)
+                return false;
+
+            if (!char.IsUpper(groupNo[0]) || !char.IsDigit(groupNo[1]) || !char.IsDigit(groupNo[2]) || !char.IsDigit(groupNo[3]))
+                return false;
+            return true;
+        }
+
+        public string GroupNo
+        {
+            get { return _groupNo; }
+            set
+            {
+                while (!CheckGroupNo(value))
+                {
+                    Console.WriteLine("Invalid GroupNo format. Please enter again!!.");
+                    Console.Write("Enter GroupNo: ");
+                    value = Console.ReadLine();
+                }
+                _groupNo = value;
+            }
+        }
+    }
+}
